948b82557f900a24e1751b571d260e41bc1b4f66
Kostas <diamantopoulos.kostas@hotmail.com>
2024-04-20 22:44:46 +0100

    added youtube autoplay

--- src/app/components/Video.tsx
+++ src/app/components/Video.tsx
@@ -1,9 +1,10 @@
 export default function Example123() {
     return (
-        <div className="container mx-auto">
+      <div className="container mx-auto">
         <div className="w-full aspect-w-16 aspect-h-9">
-        <iframe className="w-full aspect-video md:aspect-square"
-            src="https://www.youtube.com/embed/zgovnOsxnR4"
+          <iframe
+            className="w-full aspect-video md:aspect-aspect-w-16 aspect-h-9"
+            src="https://www.youtube.com/embed/zgovnOsxnR4?autoplay=1"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
