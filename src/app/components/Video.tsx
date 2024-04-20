993b13ac9943c3ab89d4007cc13bb0e4ba08fc72
Kostas <diamantopoulos.kostas@hotmail.com>
2024-04-20 23:29:07 +0100

    muted and autoplay added

--- src/app/components/Video.tsx
+++ src/app/components/Video.tsx
@@ -4,7 +4,7 @@ export default function Example123() {
         <div className="w-full aspect-w-16 aspect-h-9">
           <iframe
             className="w-full aspect-video md:aspect-aspect-w-16 aspect-h-9"
-            src="https://www.youtube.com/embed/zgovnOsxnR4?autoplay=1"
+            src="https://www.youtube.com/embed/zgovnOsxnR4?autoplay=1&mute=1&loop=1"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -13,4 +13,21 @@ export default function Example123() {
       </div>
     );
   }
+
   
+  /*
+export default function Example123() {
+    return (
+        <div className="container mx-auto">
+        <div className="w-full aspect-w-16 aspect-h-9">
+        <iframe className="w-full aspect-video md:aspect-aspect-w-16 aspect-h-9"
+            src="https://www.youtube.com/embed/zgovnOsxnR4"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
+  */
